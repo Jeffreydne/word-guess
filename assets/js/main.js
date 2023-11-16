abe344c60b0148a7663f983fb9cd9bd157b33fb3
Jeffreydne <Jeffreydne@gmail.com>
2023-11-16 11:39:23 -0800

    set up timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
 console.log("I am working");
+// DOM variables
+const timerEl = document.querySelector("#timeLeft");
+const gamesWon = document.querySelector("#wins");
+const gamesLost = document.querySelector("#losses");
+const word = document.querySelector("#word");
+const startBtn = document.querySelector("button");
+//other variables
+let timerId;
+let secondsLeft = 30
+
+
+function startTimer() {
+    timerId = setInterval(function() {
+        if(secondsLeft > 0) {
+            secondsLeft--;
+            timerEl.textContent = secondsLeft;
+        } else {
+            clearInterval(timerId);
+            timerId = null;
+            endGame();
+        }
+    }, 1000)
+}
+function startGame() {
+    startTimer();
+}
+function endGame() {
+    console.log("game is over");
+}
+
+startBtn.addEventListener("click", startGame);
